9d8fd6370203575f8a26cf02f0fff3b10a52df65
Hitesh Chauhan <ihiteshmanage@gmail.com>
2020-09-17 07:37:32 +0530

    Accepted

--- Case Study - Sprint 4/Learning Outcome 5/Admin Service/Source.java
+++ Case Study - Sprint 4/Learning Outcome 5/Admin Service/Source.java
@@ -1,148 +1,204 @@
-import java.util.Scanner;
+import java.util.*;
 
-interface AdminService {
-   public Toy searchToy(int id);
+interface AdminService{
+void addToy(Toy toy);
+void updateToy(Toy toy);
+void deleteToy(Toy toy);
+Toy[] getToys();
 }
 
-interface CustomerService {
-   public Customer searchCustomer(int id);
+class AdminServiceImpl implements AdminService{
+public static Toy[] toyArray=new Toy[5];
+public static int count=0;
+@Override
+public void addToy(Toy toy) {
+toyArray[count]=toy;
+count++;
 }
 
-class Customer {
-   private int customerId;
-   private String customerName;
-   private String email;
-   private String password;
-   private String address;
-
-   public Customer() {
-
-   }
-
-   public Customer(int customerId, String customerName, String email, String password, String address) {
-      super();
-      this.customerId = customerId;
-      this.customerName = customerName;
-      this.email = email;
-      this.password = password;
-      this.address = address;
-   }
-
-    public int getCustomerId() {
-        return this.customerId;
-    }
-
-    public String getCustomerName() {
-        return this.customerName;
-    }
-
-    public String toString() {
-        return "Id: " + this.customerId + "\nName: " + this.customerName;
-    }
+public void updateToy(Toy toy) {
+for(int i=0;i<=count;i++) {
+if(toyArray[i].getToyId()==toy.getToyId()) {
+toyArray[i]=toy;
+break;
+}
+}
+}
+public void deleteToy(Toy toy) {
+for(int i=0;i<=count;i++) {
+if(toyArray[i].getToyId()==toy.getToyId()) {
+toyArray[i]=null;
+break;
+}
+}
+}
+public Toy[] getToys() {
+return toyArray;
 }
 
-class Toy {
-   private int toyId;
-   private String toyName;
-   private String toyType;
-   private int minAge;
-   private int maxAge;
-   private double price;
-   private int quantity;
-   private int rentalAmt;
-   private int refundableDeposit;
-   public String toString;
-
-   public Toy() {
-
-   }
-
-   public Toy(int toyId, String toyName, String toyType, int minAge, int maxAge, double price, int rentalAmt,
-         int refundableDeposit, int quantity) {
-      this.toyId = toyId;
-      this.toyName = toyName;
-      this.toyType = toyType;
-      this.minAge = minAge;
-      this.maxAge = maxAge;
-      this.price = price;
-      this.rentalAmt = rentalAmt;
-      this.refundableDeposit = refundableDeposit;
-      this.quantity = quantity;
-      this.toString = "Id: " + this.toyId + "\nToyName: " + this.toyName;
-   }
-
-   public int getToyId() {
-      return this.toyId;
-   }
-
-   public String getToyName() {
-      return this.toyName;
-   }
-
-   public String toString() {
-      return "Id: " + this.toyId + "\nToyName: " + this.toyName;
-   }
 }
 
-class CustomerServiceImpl extends Customer implements CustomerService {
-   public static Customer[] customerArray = new Customer[5];
+class Toy{
+private int toyId,minAge,maxAge,quantity;
+private String toyName,toyType;
+private double price,rentalAmount,refundableDeposit;
 
-   public CustomerServiceImpl() {
-      customerArray[0] = new Customer(100, "Krithick","krithick@gmail.com","krithi","Bangalore");
-      customerArray[1] = new Customer(101, "Rajan","rajan@gmail.com","rajan#345","Bangalore");
-      customerArray[2] = new Customer(102, "Chandrav","chand@gmail.com","wel$234","Chennai");
-      customerArray[3] = new Customer(103, "Ankit","ankit@gmail.com","kit@56","Mumbai");
-      customerArray[4] = new Customer(104, "Akilan","akilan@gmail.com","ak*76","Pune");
-   }
-
-    public Customer searchCustomer(int id) {
-        for (int i = 0; i < 5; i++) {
-            if (customerArray[i].getCustomerId() == id) {
-                return customerArray[i];
-            }
-        }
-        return null;
-    }
-   
+Toy(int id,String name,String type,int min,int max,double price,int quant,double rent,double refund){
+this.toyId=id;
+this.toyName=name;
+this.toyType=type;
+this.minAge=min;
+this.maxAge=max;
+this.price=price;
+this.quantity=quant;
+this.rentalAmount=rent;
+this.refundableDeposit=refund;
 }
 
-class AdminServiceImpl implements AdminService {
-   static Toy availableToys[] = new Toy[5];
-   Toy toy = new Toy();
-
-   AdminServiceImpl() {
-      availableToys[0] = new Toy(120, "Rubber Ducky", "Toy", 1, 3, 200, 20, 20, 200);
-      availableToys[1] = new Toy(130, "Car", "Toy", 1, 5, 100, 30, 20, 100);
-      availableToys[2] = new Toy(150, "Kite", "Toy", 3, 8, 100, 50, 20, 50);
-      availableToys[3] = new Toy(180, "Airplane", "Toy", 4, 7, 500, 30, 50, 20);
-    }
-
-    public Toy searchToy(int id) {
-        for (int i = 0; i < 4; i++) {
-            if (availableToys[i].getToyId() == id) {
-                return availableToys[i];
-            }
-        }
-        return null;
-    }
+public int getToyId() {
+return toyId;
 }
 
-public class Source {
-    public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int ch=sc.nextInt();
-    int id=sc.nextInt();
-    sc.close();
-    switch(ch) {
-    case 1:  CustomerService customerService=new CustomerServiceImpl();
-       Customer customer=customerService.searchCustomer(id);
-       System.out.println(customerService.toString());
-       break;
-    case 2:  AdminService adminService=new AdminServiceImpl();
-       Toy toy=adminService.searchToy(id);
-       System.out.println(toy.toString());
-       break;
-    default: System.out.println("Invalid Choice");
-    }
+public void setToyId(int toyId) {
+this.toyId = toyId;
+}
+
+public int getMinAge() {
+return minAge;
+}
+
+public void setMinAge(int minAge) {
+this.minAge = minAge;
+}
+
+public int getMaxAge() {
+return maxAge;
+}
+
+public void setMaxAge(int maxAge) {
+this.maxAge = maxAge;
+}
+
+public int getQuantity() {
+return quantity;
+}
+
+public void setQuantity(int quantity) {
+this.quantity = quantity;
+}
+
+public String getToyName() {
+return toyName;
+}
+
+public void setToyName(String toyName) {
+this.toyName = toyName;
+}
+
+public String getToyType() {
+return toyType;
+}
+
+public void setToyType(String toyType) {
+this.toyType = toyType;
+}
+
+public double getPrice() {
+return price;
+}
+
+public void setPrice(double price) {
+this.price = price;
+}
+
+public double getRentalAmount() {
+return rentalAmount;
+}
+
+public void setRentalAmount(double rentalAmount) {
+this.rentalAmount = rentalAmount;
+}
+
+public double getRefundableDeposit() {
+return refundableDeposit;
+}
+
+public void setRefundableDeposit(double refundableDeposit) {
+this.refundableDeposit = refundableDeposit;
+}
+
+public String toString() {
+return("Toy Name: "+this.toyName+"\nToy Type: "+this.toyType+"\nQuantity: "+this.quantity+"\nRentalAmount: "+this.rentalAmount);
+}
+
+}
+
+class Source{
+public static void main(String args[]) {
+Scanner s = new Scanner(System.in);
+int choice=s.nextInt();
+int id,min,max,quantity;
+String name,type;
+double price,rent,refund;
+Toy[] toys;
+switch(choice) {
+case 1:
+   if(s.hasNext()){
+id=s.nextInt();
+name = s.next();
+type=s.next();
+min=s.nextInt();
+max=s.nextInt();
+price=s.nextDouble();
+quantity=s.nextInt();
+rent=s.nextDouble();
+refund=s.nextDouble();
+Toy t= new Toy(id,name,type,min,max,price,quantity,rent,refund);
+AdminService adm = new AdminServiceImpl();
+adm.addToy(t);
+toys=adm.getToys();
+for(int i=0;i<toys.length;i++) {
+System.out.println(adm.toString());
+}
    }
+break;
+case 2:
+id=s.nextInt();
+name = s.next();
+type=s.next();
+min=s.nextInt();
+max=s.nextInt();
+price=s.nextDouble();
+quantity=s.nextInt();
+rent=s.nextDouble();
+refund=s.nextDouble();
+Toy t1= new Toy(id,name,type,min,max,price,quantity,rent,refund);
+AdminService adm1 = new AdminServiceImpl();
+adm1.updateToy(t1);
+System.out.println(adm1.toString());
+break;
+case 3:
+id=s.nextInt();
+name = s.next();
+type=s.next();
+min=s.nextInt();
+max=s.nextInt();
+price=s.nextDouble();
+quantity=s.nextInt();
+rent=s.nextDouble();
+refund=s.nextDouble();
+Toy t2= new Toy(id,name,type,min,max,price,quantity,rent,refund);
+AdminService adm2 = new AdminServiceImpl();
+adm2.deleteToy(t2);
+break;
+case 4:
+
+AdminService adm3 = new AdminServiceImpl();
+toys=adm3.getToys();
+for(int i=0;i<toys.length;i++) {
+System.out.println(adm3.toString());
+}
+}
+s.close();
+}
 }
