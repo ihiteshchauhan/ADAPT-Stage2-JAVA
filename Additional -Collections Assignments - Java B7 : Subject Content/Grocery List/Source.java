4597d19dee97b68be7b780b3b8f9f973a217f414
Hitesh Chauhan <ihiteshmanage@gmail.com>
2020-09-26 11:20:32 +0530

    100 points [Accepted]

--- Additional -Collections Assignments - Java B7 : Subject Content/Grocery List/Source.java
+++ Additional -Collections Assignments - Java B7 : Subject Content/Grocery List/Source.java
@@ -28,22 +28,5 @@ public class Source {
         return groceryList.size();
         
     }
-    
-    public static void main(String args[]) {
-        ArrayList<String> groceryList = new ArrayList<String>();
-        String mod, rem, search;
-        Source s = new Source();
-       int size;
-       s.addItem(groceryList, "Fruit");
-       s.addItem(groceryList, "Vegetable");
-       s.addItem(groceryList, "Beverages");
-       mod = s.modifyItem(groceryList, 1, "Fruitss");
-       System.out.println(mod);
-       rem = s.removeItem(groceryList, 1);
-       System.out.println(rem);
-       search = s.searchItem(groceryList, "Beverages");
-       System.out.println(search);
-       size = s.getSize(groceryList);
-       System.out.println(size);
-    }   
+  
 }
