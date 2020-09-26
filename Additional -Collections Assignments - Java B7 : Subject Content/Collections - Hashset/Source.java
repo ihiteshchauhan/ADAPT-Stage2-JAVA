282d9578c6be9cb8630a0f483512c2969221db52
Hitesh Chauhan <ihiteshmanage@gmail.com>
2020-09-26 11:16:41 +0530

    100 points [Accepted]

--- Additional -Collections Assignments - Java B7 : Subject Content/Collections - Hashset/Source.java
+++ Additional -Collections Assignments - Java B7 : Subject Content/Collections - Hashset/Source.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class Source {
     public Set<Integer> union(Set<Integer> a, Set<Integer> b){
         Set<Integer> union = new HashSet<Integer>(a);  
@@ -16,17 +17,4 @@ public class Source {
         intersection.retainAll(b);  
         return intersection;
     }
-    
-   public static void main(String[] args){
-       HashSet<Integer> set1 = new HashSet();
-        set1.add(2);
-        set1.add(4);
-        HashSet<Integer> set2 = new HashSet();
-        set2.add(9);
-        set2.add(2);
-        Source s = new Source();
-        System.out.println(s.subtract(set1, set2));
-        System.out.println(s.union(set1, set2));
-        System.out.println(s.intersect(set1, set2));
-   }
 }
